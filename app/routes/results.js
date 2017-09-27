861ba1077d8110ec1cf635b200c05c8148300251
Granton Mchana <gmchana254@gmail.com>
2017-09-27 11:39:41 +0300

    aditions

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 
 export default Ember.Route.extend({
  model: function(params) {
    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=0613f3c5dde44d699a1a8c7adb2e6ed7 &zip=' + params.zip;
+   var key = config.myApiKey;
    return Ember.$.getJSON(url).then(function(responseJSON) {
      return responseJSON.results;
    });
